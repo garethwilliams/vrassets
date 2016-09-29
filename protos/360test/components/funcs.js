6a9dc8955777bad931778cdaa89eb5c13a4424bd
Gareth Williams <cockneybert@gmail.com>
2016-09-29 17:27:51 +0100

    first scene switcher working - needs tidy!

---
+++ protos/360test/components/funcs.js
@@ -0,0 +1,20 @@
+// mainScene
+
+// document.getElementById("mainScene").style.visible = "visible";
+
+
+// var element = document.getElementById("mainScene"); 
+// element.setAttribute("visible", "false")
+
+// $('mainScene').attr('visible','false')
+
+
+
+
+
+
+document.getElementById("oneSub").onclick = function() { 
+	alert("testString");
+	// var element = document.getElementById("mainScene"); 
+	// element.setAttribute("visible", "false")
+};
